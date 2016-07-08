6be9ed643d6ba8f383edec70abc34e7fd74a1f08
tylerlee <tyler@orgsync.com>
2016-07-07 22:00:13 -0400

    added awesome.jsx

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
+import Awesome from './awesome.jsx';
 import React from 'react';
 import {render} from 'react-dom';
 
 class App extends React.Component {
   render () {
-    return <p> Hello React!</p>;
+    return (
+      <div>
+        <p>Hi World</p>
+        <Awesome />
+      </div>
+    )
   }
 }
 
