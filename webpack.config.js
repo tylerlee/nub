58fb827177f5ea9ae2ea8d88eae6093c26a848da
tylerlee <tyler@orgsync.com>
2016-07-07 22:21:47 -0400

    code style

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,42 @@
+const path = require('path');
 const webpack = require('webpack');
-const APP_DIR = './assets';
-const APP_DEST = './public';
+
+const PATHS = {
+  app: path.join(__dirname, 'assets'),
+  build: path.join(__dirname, 'public')
+};
 
 module.exports = {
-    entry: APP_DIR + '/app.js',
-    output: {
-        path: APP_DEST,
-        filename: 'app.bundle.js',
+  entry: PATHS.app + '/app.js',
+  output: {
+    path: PATHS.build,
+    filename: 'app.bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
     },
-    module: {
-        loaders: [{
-            test: /.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        },
-        {
-          test : /\.jsx?/,
-          include : APP_DIR,
-          loader : 'babel'
-        }
-      ]
+    {
+      test : /\.jsx?/,
+      include : PATHS.app,
+      loader : 'babel'
     },
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false,
-        },
-        output: {
-          comments: false,
-        },
+    {
+      test: /\.css$/,
+      loaders: ['style', 'css'],
+      include: PATHS.style
+    }]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+      output: {
+        comments: false,
+      },
     }),
   ]
 }
